6c9b6e62f21867f3a409a24aa539c82627d98c0c
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2020-05-28 22:03:20 +0800

    大写字母转化为小写，并输出到屏幕上，未完成

---
+++ 6 - toLowerAndPrint/toLowerAndPrint.asm
@@ -0,0 +1,40 @@
+DATAS SEGMENT
+    string DB 'AbcdE3a79G'
+    a DB 'A'
+    z DB 'Z'
+    s DB 'a' - 'A'
+    ;此处输入数据段代码
+DATAS ENDS
+
+STACKS SEGMENT
+    ;此处输入堆栈段代码
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+    MOV AX,DATAS
+    MOV DS,AX
+    MOV CX, 10
+    MOV AX, 0
+    MOV SI, 0
+to_lower:
+	MOV AL, string[SI]
+	CMP AL, a
+	JB increment;待转换字符小于A的ASCII码值，跳转到下标移动语句
+	CMP AL, z
+	JA increment;待转换字符大于Z的ASCII码值，跳转到下标移动语句
+	ADD AL, s;待转换字符是大写字母，则将其加上'a'和'A'的ASCII码值的差值转换成小写字母
+	MOV DL, AL;将待输出字符存到DL中
+	MOV AH, 2
+	INT 21H
+	
+increment:
+	ADD SI, 1
+	LOOP to_lower
+	
+    MOV AH,4CH
+    INT 21H
+CODES ENDS
+    END START
+
